23893c358f3fdc8df289c4d80c455d5febd40287
ivoazv <ivobastos82@gmail.com>
2022-08-23 03:22:57 +0000

    '-crypt' na criaçao do usuario editado para '-1

--- servidor.sh
+++ servidor.sh
@@ -13,17 +13,17 @@ echo "Grupos criados"
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM 
+useradd carlos -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_ADM 
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 senha123) -G GRP_SEC
 echo "Usuários criados"
 
 echo "Especificando permissões..."
